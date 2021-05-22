e65117389a950ede7e07f9122f300a4902d16016
Rafael Martins <rafael.martins92@outlook.com>
2021-05-22 12:25:08 -0300

    feat: create component Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,41 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Search,
+  Menu,
+  IconUser,
+  TextUser,
+  IconCart,
+  Hamburguer,
+  IconSearch,
+} from "./styles";
+
+import logo from "../../assets/logo-corebiz-preto-cinza.svg";
+import iconSearch from "../../assets/magnifying-glass.svg";
+import iconUser from "../../assets/user.svg";
+import iconCart from "../../assets/Minicart.svg";
+import hamburguer from "../../assets/Icon.svg";
 
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Container>
+      <Content>
+        <Hamburguer src={hamburguer} alt="Menu" />
+        <img src={logo} alt="Logo" />
+        <Search>
+          O que esta procurando?
+          <IconSearch src={iconSearch} alt="Buscar" />
+        </Search>
+        <Menu>
+          <IconUser src={iconUser} alt="Minha Conta" />
+          <TextUser>Minha Conta</TextUser>
+          <IconCart src={iconCart} alt="Meu Carrinho" />
+        </Menu>
+      </Content>
+    </Container>
+  );
 }
 
 export default Header;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,68 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 80px;
+  background: var(--color-bg-second);
+  display: flex;
+`;
+
+export const Content = styled.div`
+  max-width: 1240px;
+  margin: 0 auto;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Search = styled.div`
+  border-bottom: 1px solid var(--color-fourth);
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 18px;
+  color: var(--color-fourth);
+
+  @media screen and (max-width: 960px) {
+    order: 5;
+    margin-right: 20px;
+  }
+`;
+
+export const IconSearch = styled.img`
+  margin-bottom: 5px;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+`;
+
+export const IconUser = styled.img`
+  margin-right: 5px;
+
+  @media screen and (max-width: 960px) {
+    display: none;
+  }
+`;
+
+export const TextUser = styled.p`
+  @media screen and (max-width: 960px) {
+    display: none;
+  }
+`;
+
+export const IconCart = styled.img`
+  margin: 0 20px;
+`;
+
+export const Hamburguer = styled.img`
+  display: none;
+
+  @media screen and (max-width: 960px) {
+    display: block;
+    margin: 0 20px;
+  }
+`;
