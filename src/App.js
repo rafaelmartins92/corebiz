c94fbabcc6d5ed6ce615ad5e8901f7df906a44d8
Rafael Martins <rafael.martins92@outlook.com>
2021-05-22 08:45:44 -0300

    chore: create initial structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import GlobalStyle from "./styles/global";
 
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
